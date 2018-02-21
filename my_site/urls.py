6b2e013484a454b1c0757edab883b0c94c25979c
James Maher <maher.j@live.com>
2018-02-21 11:41:45 +0000

    Fixes to django url import

--- my_site/urls.py
+++ my_site/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from my_site import site
+from . import site
 
 urlpatterns = [
     path('', site.index, name='index'),
